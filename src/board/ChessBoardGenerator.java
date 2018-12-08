660c12f4c4da9f1c3e7bd4809fecaf5ff26f7875
MrPierroo <ps.15895@hotmail.fr>
2018-12-08 20:36:44 +0100

    Structure

--- src/board/ChessBoardGenerator.java
+++ src/board/ChessBoardGenerator.java
@@ -1,19 +1,56 @@
 package board;
 
+import java.util.Arrays;
+
+import move.Moves;
+
 public class ChessBoardGenerator {
 
+	public static long WP=0L;
+	public static long WN=0L;
+	public static long WB=0L;
+	public static long WR=0L;
+	public static long WQ=0L;
+	public static long WK=0L;
+	public static long BP=0L;
+	public static long BN=0L;
+	public static long BB=0L;
+	public static long BR=0L;
+	public static long BQ=0L;
+	public static long BK=0L;
+	public static long EP=0L;
+	public static boolean CWK = true, CWQ = true, CBK = true, CBQ = true, WhiteToMove = true;
+	public static long UniversalWP = 0L, UniversalWN = 0L, UniversalWB = 0L, UniversalWR = 0L,
+			UniversalWQ = 0L, UniversalWK = 0L, UniversalBP = 0L, UniversalBN = 0L,
+			UniversalBB = 0L, UniversalBR = 0L, UniversalBQ = 0L, UniversalBK = 0L,
+			UniversalEP = 0L;
+	public static boolean UniversalCastleWK = true, UniversalCastleWQ = true,
+			UniversalCastleBK = true, UniversalCastleBQ = true;
+
+	// Initialisation du plateau de jeu
 	public static void initiateChessBoard() {
-		// TODO Auto-generated method stub
-		
+		String chessBoard[][]={
+				{"r","n","b","q","k","b","n","r"},
+				{"p","p","p","p","p","p","p","p"},
+				{" "," "," "," "," "," "," "," "},
+				{" "," "," "," "," "," "," "," "},
+				{" "," "," "," "," "," "," "," "},
+				{" "," "," "," "," "," "," "," "},
+				{"P","P","P","P","P","P","P","P"},
+				{"R","N","B","Q","K","B","N","R"}};
 	}
 
-	public static void importFEN(String input) {
-		// TODO Auto-generated method stub
-		
-	}
 
 	public static void movePiece(String move) {
-		// TODO Auto-generated method stub
+
+	}
+
+	public static void readMove(String move) {
+		
+	}
+
+
+	public static void print() {
 		
 	}
 
--- src/communication/UCI.java
+++ src/communication/UCI.java
@@ -11,11 +11,11 @@ import move.Moves;
  */
 
 public class UCI {
-    
+
 	// Configurations
 	static String ENGINENAME="Nurblag Chess";
 	static String AUTHORS = "Benoit Manhes, Lilian Pattier, Pierre Scalzo";
-	
+
 	// Entrees possibles
 	static String UCI = "uci";
 	static String OPTIONS = "setoption";
@@ -28,122 +28,141 @@ public class UCI {
 	static String GO = "go";
 	static String QUIT = "quit";
 	static String PRINT = "print";
-   
+
 	public static void uciCommunication() {
-        
+
 		Scanner input = new Scanner(System.in);
-        
+
 		// Boucle infinie pour toute la duree d'une partie
 		while (true) {
-			
+
 			// Lecture de l'entree
-            String inputString = input.nextLine();
-            
-            if (UCI.equals(inputString)) {
-                inputUCI();
-            }
-            
-            else if (inputString.startsWith(OPTIONS)) {
-                inputSetOption(inputString);
-            }
-            
-            else if (ISREADY.equals(inputString)) {
-                inputIsReady();
-            }
-            
-            else if (NEWGAME.equals(inputString)) {
-                inputUCINewGame();
-            }
-            
-            else if (inputString.startsWith(POSITION)) {
-                inputPosition(inputString);
-            }
-            
-            else if (inputString.startsWith(GO)) {
-                inputGo();
-            }
-            
-            else if (inputString.equals(QUIT)) {
-                inputQuit();
-                break;
-            }
-            
-            else if (PRINT.equals(inputString)) {
-                inputPrint();
-            }
-            
-        }
-		
+			String inputString = input.nextLine();
+
+			if (UCI.equals(inputString)) {
+				inputUCI();
+			}
+
+			else if (inputString.startsWith(OPTIONS)) {
+				inputSetOption(inputString);
+			}
+
+			else if (ISREADY.equals(inputString)) {
+				inputIsReady();
+			}
+
+			else if (NEWGAME.equals(inputString)) {
+				inputUCINewGame();
+			}
+
+			else if (inputString.startsWith(POSITION)) {
+				inputPosition(inputString);
+			}
+
+			else if (inputString.startsWith(GO)) {
+				inputGo();
+			}
+
+			else if (inputString.equals(QUIT)) {
+				inputQuit();
+				break;
+			}
+
+			else if (PRINT.equals(inputString)) {
+				inputPrint();
+			}
+
+			else {
+				System.out.println("Commande incorrecte");
+			}
+
+		}
+
 		input.close();
+	}
+
+	public static void inputUCI() {
+		System.out.println("id name "+ENGINENAME);
+		System.out.println("id author "+AUTHORS);
+		//options go here
+		System.out.println("uciok");
+	}
+	
+	public static void inputSetOption(String inputString) {
+	}
+	
+	public static void inputIsReady() {
+		System.out.println("readyok");
+	}
+	
+	public static void inputUCINewGame() {
+        ChessBoardGenerator.initiateChessBoard();
     }
 	
-    public static void inputUCI() {
-        System.out.println("id name "+ENGINENAME);
-        System.out.println("id author "+AUTHORS);
-        //options go here
-        System.out.println("uciok");
-    }
-    public static void inputSetOption(String inputString) {
-        //set options
-    }
-    public static void inputIsReady() {
-         System.out.println("readyok");
-    }
-    public static void inputUCINewGame() {
-        //TODO
-    }
-    public static void inputPosition(String input) {
-    	
-    	// Enlever la commande "position "
-    	input = input.substring(POSITION.length()+1).concat(" ");
-        
-    	if (input.contains(STARTPOSITION)) {
-            input = input.substring(STARTPOSITION.length()+1);
-            ChessBoardGenerator.initiateChessBoard();
-        }
-        else if (input.contains(FEN)) {
-            input = input.substring(FEN.length()+1);
-            ChessBoardGenerator.importFEN(input);
-        }
+	public static void inputPosition(String input) {
 
-    	// Prendre en consideration l'ensemble des mouvements ordonnes
-        if (input.contains(MOVES)) {
-            input = input.substring(input.indexOf(MOVES) + MOVES.length()+1);
-            while (input.length() > 0) {
-            	String moves;
-                moves = makeMove(input);
-                input=input.substring(input.indexOf(' ')+1);
-            }
-        }
-    }
+		// Gerer l'erreur d'une mauvaise entree (uniquement pour les tests sur console)
+		if(input.length() == 8) {
+		}
+
+		else {
+			
+			input = input.substring(POSITION.length()+1).concat(" ");
+
+			boolean accepted = false;
+			if (input.contains(STARTPOSITION)) {
+				input = input.substring(input.indexOf(STARTPOSITION) + STARTPOSITION.length()+1);
+				//ChessBoardGenerator.importFEN("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1");
+				ChessBoardGenerator.initiateChessBoard();
+				accepted = true;
+			}
+
+			// Prendre en consideration l'ensemble des mouvements ordonnes
+			if (input.contains(MOVES)) {
+				input = input.substring(input.indexOf(MOVES) + MOVES.length()+1);
+				
+				while (input.length() > 0) {
+					String move;
+					// Nous nous interessons au premier mouvement de la String
+					move = input.substring(0,4);
+					// Nous le traitons
+					ChessBoardGenerator.readMove(move);
+					// Nous enlevons ce mouvement a la String puisqu'il a ete traite
+	                input = input.substring(input.indexOf(' ')+1);
+				}
+				accepted = true;
+			}
+			
+			else if(accepted == false) {
+			}
+		}
+
+		
+	}
+
+	// Calculer le meilleur mouvement pour jouer
+	public static void inputGo() {
+		String move = "b2b4";
+		ThreadHandler.calculateBestMove(); // => retourne un mouvement qu'il faut maintenant system.out
+		System.out.println("bestmove "+move);
+	}
+
+	// Quitter l'uci
+	public static void inputQuit() {
+		System.exit(0);
+	}
+
+	// Mettre a jour l'affichage
+	public static void inputPrint() {
+		ChessBoardGenerator.print();
+	}
+
+
+	
+	/* ---------------------------------------------------------------------------------------------------------------------------------------------------
+	 * ----------------------------------------------------------------- Methodes utiles ----------------------------------------------------------------- 
+	 * ---------------------------------------------------------------------------------------------------------------------------------------------------
+	*/ 
+
     
-    // Calculer le meilleur mouvement avant de jouer
-    public static void inputGo() {
-    	ThreadHandler.calculateBestMove();
-    }
-    
-    // Quitter l'uci
-    public static void inputQuit() {
-        System.exit(0);
-    }
-    
-    // Mettre a jour l'affichage
-    public static void inputPrint() {
-        
-    }
-    
-    
-    /*
-     * Methodes utiles
-     */
-    
-    private static String makeMove(String input) {
-        int moveFrom_vertical = (input.charAt(0) - 'a');
-        int moveFrom_horizontal = ('8' - input.charAt(1));
-        int moveTo_vertical = input.charAt(2) - 'a';
-        int moveTo_horizontal = '8' - input.charAt(3);
-        String move = Integer.toString(moveFrom_horizontal) + Integer.toString(moveFrom_vertical) + Integer.toString(moveTo_horizontal) + Integer.toString(moveTo_vertical);
-        ChessBoardGenerator.movePiece(move);
-        return move;
-    }
 }
