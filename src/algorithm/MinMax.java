13f2a1c84203f95786b33ca8d1ed04916d909367
MrPierroo <ps.15895@hotmail.fr>
2018-12-11 21:29:57 +0100

    Tentative

--- src/algorithm/MinMax.java
+++ src/algorithm/MinMax.java
@@ -23,43 +23,50 @@ public class MinMax {
 
 	/////////////// algo min max avec alpha beta ////////////////////////////////////////////////
 	private int minmax(Board board, int depth, int alpha, int beta, boolean maximizingPlayer, boolean player) {
-		
-		if (0 == depth || board.gameOver() ) return Evaluation.evaluate(board, player);
 
 		int eval;
 		int maxEval;
 		int minEval;
 
-		// algorithme max
-		if (maximizingPlayer) {
-			maxEval = -1000000;
+		if(!board.gameOver() && depth>0) {
+			// algorithme max
+			if (maximizingPlayer) {
+				maxEval = -1000000;
 
-			for (String m : Moves.legalMove(board, player)) {
-				board.move(m);
-				eval = minmax(board, depth - 1, alpha, beta , false, player); 
-				if(eval > maxEval && depth == DEPTH) this.bestMove = m;	// sauvegarde du meilleur mouvement
-				maxEval = Math.max(maxEval, eval);
-				alpha = Math.max(alpha, eval);
-				board.undoMove(m);
-				if (beta <= alpha)
-					break;// so cut here (pruning)
-			}
-			return maxEval;	
+				for (String m : Moves.legalMove(board, player)) {
+					board.move(m);
+					eval = minmax(board, depth - 1, alpha, beta , false, player); 
+					if(eval > maxEval && depth == DEPTH) this.bestMove = m;	// sauvegarde du meilleur mouvement
+					maxEval = Math.max(maxEval, eval);
+					alpha = Math.max(alpha, eval);
+					board.undoMove(m);
+					if (beta <= alpha)
+						break;// so cut here (pruning)
+				}
+				return maxEval;	
 
-		// algorithme min
-		}else {
-			minEval = 1000000;
-			for (String m : Moves.legalMove(board, !player)) {
-				board.move(m);
-				eval = minmax(board, depth - 1, alpha, beta , true, player);
-				minEval = Math.min(minEval, eval);
-				beta = Math.min(beta, eval);
-				board.undoMove(m);
-				if(beta <= alpha)
-					break;
+			// algorithme min
 			}
-			return minEval;
+			else {
+				minEval = 1000000;
+				for (String m : Moves.legalMove(board, !player)) {
+					board.move(m);
+					eval = minmax(board, depth - 1, alpha, beta , true, player);
+					minEval = Math.min(minEval, eval);
+					beta = Math.min(beta, eval);
+					board.undoMove(m);
+					if(beta <= alpha)
+						break;
+				}
+				return minEval;
+			}
 		}
+		
+		else {
+			return Evaluation.evaluate(board, player);
+		}
+		
+		
 	}
 
 	//////////////////////// getter /////////////////////////////////////////////////////////////
